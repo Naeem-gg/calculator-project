7c52ddd3c31ad73beed9bf55cd84b1aa192d1970
Naeemgg <naeemgg@duck.com>
2022-09-09 00:49:03 +0530

    completed simple functions

--- calculator.js
+++ calculator.js
@@ -1,11 +1,22 @@
 const express = require("express");
+// const bodyParser = require("body-parser");
 const app = express();
-
-app.listen(3000,function(){
+app.use(express.urlencoded({extended:true}));
 
 app.get("/",(req,res)=>{
-    console.log("running with nodemon...")
-    res.send("Hello World!");
+    res.sendFile(`${__dirname}/index.html`);
+});
+app.post("/",(req,res)=>{
+    // console.log(req.body);
+    let num1 = Number(req.body.num1);
+    let num2 = Number(req.body.num2);
+
+    let add = num1 + num2;
+
+    res.send("Kya bat hai bassa, le addition: "+add);
+    res.end();
 })
+app.listen(3000,function(){
+    console.log("running with nodemon...")
 
 });
