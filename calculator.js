887a2637ed42c5bfa8d9974b35b706dad0edfa13
Naeemgg <naeemgg@duck.com>
2022-10-01 00:27:07 +0530

    done it again

--- calculator.js
+++ calculator.js
@@ -1,22 +1,35 @@
 const express = require("express");
-// const bodyParser = require("body-parser");
 const app = express();
-app.use(express.urlencoded({extended:true}));
 
+app.use(express.urlencoded({extended:true}));
 app.get("/",(req,res)=>{
-    res.sendFile(`${__dirname}/index.html`);
+
+    res.sendFile(__dirname+"/index.html");
 });
+
 app.post("/",(req,res)=>{
-    // console.log(req.body);
     let num1 = Number(req.body.num1);
     let num2 = Number(req.body.num2);
+    let sum = num1 + num2;
 
-    let add = num1 + num2;
+// console.log(req.body);
+res.send(`<p>Thanks dude. heres your sum<br> sum = ${sum}</p>`);
+console.log("This is console logs");
 
-    res.send("Kya bat hai bassa, le addition: "+add);
-    res.end();
-})
-app.listen(3000,function(){
-    console.log("running with nodemon...")
+});
 
+app.get("/bmicalculator",(req,res) =>{
+    res.sendFile(__dirname+"/bmi.html");
+});
+app.post("/bmicalculator", (req, res) => {
+    console.log(req.body);
+    let height = Number(req.body.height);
+    let weight = Number(req.body.weight);
+    let bmi = weight / (height * height);
+    res.send(`Thanks man<br> this is your bmi [BMI = ${bmi}]`);
+
+});
+app.listen("3000", () =>{
+
+    console.log("express server is up and running by nodemon at port 3000");
 });
